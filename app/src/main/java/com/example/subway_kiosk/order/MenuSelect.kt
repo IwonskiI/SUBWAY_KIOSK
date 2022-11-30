9b7f76470dd31d6a4da5268fcade35ca4f492085
김진현 <kimjhyun0627@gmail.com>
2022-11-30 18:37:27 +0900

    update menuSelect.kt

--- app/src/main/java/com/example/subway_kiosk/order/MenuSelect.kt
+++ app/src/main/java/com/example/subway_kiosk/order/MenuSelect.kt
@@ -5,10 +5,15 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import com.example.subway_kiosk.R
 import com.example.subway_kiosk.util.Sandwich
 import com.example.subway_kiosk.util.Vegetable
+import java.lang.Integer.min
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class MenuSelect : AppCompatActivity()
 {
@@ -18,36 +23,57 @@ class MenuSelect : AppCompatActivity()
         setContentView(R.layout.menu_select)
 
         val meatSelectView = View.inflate(this, R.layout.meat_select, null)
-        val menuList = loadSandwichData(meatSelectView)
+        val menuList = Sandwich.loadSandwichData(meatSelectView)
 
         if (intent.hasExtra("selectedMeat"))
         {
             val mainTextView = findViewById<TextView>(R.id.menuselect_text)
             val selectedMeatTextView1 = findViewById<TextView>(R.id.menuselect_meat1)
             val selectedMeatTextView2 = findViewById<TextView>(R.id.menuselect_meat2)
-            val meatGridLayout = findViewById<GridLayout>(R.id.menuselect_meat1_grid)
+            val selectedMeatGridLayout1 = findViewById<GridLayout>(R.id.menuselect_meat1_grid)
+            val selectedMeatGridLayout2 = findViewById<GridLayout>(R.id.menuselect_meat2_grid)
+            val selectedMeatButtonList1 = arrayListOf<Button>(
+                findViewById(R.id.button101),
+                findViewById(R.id.button102),
+                findViewById(R.id.button103)
+            )
+            val selectedMeatButtonList2 = arrayListOf<Button>(
+                findViewById(R.id.button201),
+                findViewById(R.id.button202),
+                findViewById(R.id.button203),
+            )
+            selectedMeatButtonList1.forEach { it.isVisible = false }
+            selectedMeatButtonList2.forEach { it.isVisible = false }
             selectedMeatTextView2.isVisible = false
-            meatGridLayout.isVisible = false
+            selectedMeatGridLayout2.isVisible = false
 
-            val selected = intent.getIntegerArrayListExtra("selectedMeat")
+            var selected = intent.getIntegerArrayListExtra("selectedMeat")
             if (selected != null)
             {
                 val menuSelectText: String
 
                 if (selected.size == 2)
                 {
+                    if (menuList.getValue(selected[0]).size < menuList.getValue(selected[1]).size) Collections.swap(
+                        selected, 0, 1
+                    )
+
                     menuSelectText = buildString {
                         append("${meatSelectView.findViewById<Button>(selected.get(0)).text}, ")
                         append("${meatSelectView.findViewById<Button>(selected.get(1)).text}")
                         append("에 대한 검색 결과입니다")
                     }
-                    selectedMeatTextView2.setText(meatSelectView.findViewById<Button>(selected.get(1)).text)
-                    selectedMeatTextView2.isVisible = true
-                    meatGridLayout.isVisible = true
 
-                    menuList.getValue(selected.get(1)).forEach { it.showInfo() }
-                    var btn = Button(this)
-                    meatGridLayout.addView(btn)
+                    selectedMeatTextView2.setText(meatSelectView.findViewById<Button>(selected.get(1)).text)
+
+                    for (i: Int in 0 .. min(3, menuList.getValue(selected.get(1)).size - 1))
+                    {
+                        selectedMeatButtonList2[i].setText(menuList.getValue(selected.get(1))[i].getSandwichName())
+                        selectedMeatButtonList2[i].isVisible = true
+                    }
+
+                    selectedMeatTextView2.isVisible = true
+                    selectedMeatGridLayout2.isVisible = true
                 }
                 else menuSelectText = buildString {
                     append("${meatSelectView.findViewById<Button>(selected.get(0)).text}")
@@ -55,6 +81,15 @@ class MenuSelect : AppCompatActivity()
                 }
 
                 menuList.getValue(selected.get(0)).forEach { it.showInfo() }
+                for (i: Int in 0 .. min(6, menuList.getValue(selected.get(0)).size - 1))
+                {
+                    selectedMeatButtonList1[i].setText(menuList.getValue(selected.get(0))[i].getSandwichName())
+                    selectedMeatButtonList1[i].setCompoundDrawablesWithIntrinsicBounds(
+                        null, getDrawable(R.drawable.meat_select_bacon_xml), null, null
+                    )
+                    selectedMeatButtonList1[i].isVisible = true
+                }
+
                 selectedMeatTextView1.setText(meatSelectView.findViewById<Button>(selected.get(0)).text)
                 mainTextView.setText(menuSelectText)
             }
@@ -65,59 +100,5 @@ class MenuSelect : AppCompatActivity()
             val nextIntent = Intent(this, MeatSelect::class.java)
             startActivity(nextIntent)
         }
-    }
-
-    fun loadSandwichData(v:View) : HashMap<Int, ArrayList<Sandwich>>
-    {
-        val menuList = hashMapOf<Int, ArrayList<Sandwich>>(
-            v.findViewById<Button>(R.id.meatSelect_ham).id to arrayListOf<Sandwich>(
-                Sandwich(
-                    101,
-                    "이탈리안 비엠티",
-                    1,
-                    1,
-                    Vegetable(true, true, true, true, true, true, true, true, false),
-                    arrayListOf<Int>(301, 101)
-                ), Sandwich(
-                    102,
-                    "써브웨이 클럽",
-                    1,
-                    1,
-                    Vegetable(true, true, true, true, true, true, true, true, false),
-                    arrayListOf<Int>(301, 101)
-                ), Sandwich(
-                    103,
-                    "햄",
-                    1,
-                    1,
-                    Vegetable(true, true, true, true, true, true, true, true, false),
-                    arrayListOf<Int>(301, 101)
-                ), Sandwich(
-                    103,
-                    "햄2",
-                    1,
-                    1,
-                    Vegetable(true, true, true, true, true, true, true, true, false),
-                    arrayListOf<Int>(301, 101)
-                )
-            ), v.findViewById<Button>(R.id.meatSelect_bacon).id to arrayListOf<Sandwich>(
-                Sandwich(
-                    201,
-                    "베이컨 비엠티",
-                    1,
-                    1,
-                    Vegetable(true, true, true, true, true, true, true, true, false),
-                    arrayListOf<Int>(301, 101)
-                ), Sandwich(
-                    202,
-                    "베이컨 클럽",
-                    1,
-                    1,
-                    Vegetable(true, true, true, true, true, true, true, true, false),
-                    arrayListOf<Int>(301, 101)
-                )
-            )
-        )
-        return menuList
     }
 }
