9201c476d6b31f2281230957a0b5ad3be7b24e4d
Minj1031 <109937806+Minj1031@users.noreply.github.com>
2022-12-13 23:13:42 +0900

    manager_stock

--- app/src/main/java/com/example/subway_kiosk/manager/ManagerStockBread.kt
+++ app/src/main/java/com/example/subway_kiosk/manager/ManagerStockBread.kt
@@ -2,33 +2,37 @@ package com.example.subway_kiosk.manager
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import com.example.subway_kiosk.R
-import com.example.subway_kiosk.order.MenuSelect
-import com.example.subway_kiosk.order.VegeSelect
-import com.example.subway_kiosk.util.Sandwich
-import kotlinx.coroutines.selects.select
-import kotlin.system.exitProcess
 import com.example.subway_kiosk.databinding.ManagerStockBreadBinding
-import com.example.subway_kiosk.manager.ManagerStockMeat
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import com.example.subway_kiosk.util.Stock
+
 
 class ManagerStockBread : AppCompatActivity()
 {
+    var RootRef = Firebase.database.reference
+    var stockRef = RootRef.child("stock")
     //get firebase
-    var white_num: Int = 3;
-    var honey_num: Int = 4;
-    var wheet_num: Int = 5;
-    var pamasan_num: Int = 6;
-    var hati_num: Int = 7;
-    var flat_num: Int = 8;
-    var american_num: Int = 9;
-    var mozzarella_num: Int = 2;
-    var shred_num: Int = 1;
+    var bread_num = Array<Int?>(7,{0});
+    var cheese_num = Array<Int?>(4,{0});
+    var white_num: Int? = 0;
+    var honey_num: Int? = 0;
+    var wheet_num: Int? = 0;
+    var pamasan_num: Int? = 0;
+    var hati_num: Int? = 0;
+    var flat_num: Int? = 0;
+    var american_num: Int? = 0;
+    var mozzarella_num: Int? = 0;
+    var shred_num: Int? = 0;
+    var cnt: Int = 0;
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
@@ -37,124 +41,164 @@ class ManagerStockBread : AppCompatActivity()
 
         setContentView(binding.root)
 
-        binding.whiteStock.text = white_num.toString().plus("개");
-        binding.honeyStock.text = honey_num.toString().plus("개");
-        binding.wheetStock.text = wheet_num.toString().plus("개");
-        binding.pamasanStock.text = pamasan_num.toString().plus("개");
-        binding.hatiStock.text = hati_num.toString().plus("개");
-        binding.flatStock.text = flat_num.toString().plus("개");
-        binding.americanStock.text = american_num.toString().plus("개");
-        binding.mozzarellaStock.text = mozzarella_num.toString().plus("개");
-        binding.shredStock.text = shred_num.toString().plus("개");
+        stockRef.child("bread").addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (item in snapshot.children) {
+                    var bread : Stock? = item.getValue(Stock::class.java)
+                    bread_num[cnt++] = bread?.num;
+                }
+                cnt = 0;
+                flat_num = bread_num[0];
+                hati_num = bread_num[1];
+                honey_num = bread_num[2];
+                pamasan_num = bread_num[3];
+                wheet_num = bread_num[4];
+                white_num = bread_num[5];
 
+                binding.whiteStock.text = white_num.toString().plus("개");
+                binding.honeyStock.text = honey_num.toString().plus("개");
+                binding.wheetStock.text = wheet_num.toString().plus("개");
+                binding.pamasanStock.text = pamasan_num.toString().plus("개");
+                binding.hatiStock.text = hati_num.toString().plus("개");
+                binding.flatStock.text = flat_num.toString().plus("개");
+            }
+            override fun onCancelled(error: DatabaseError) {
+                print(error.message)
+            }
+        } )
+
+        stockRef.child("cheese").addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (item in snapshot.children) {
+                    var cheese : Stock? = item.getValue(Stock::class.java)
+                    cheese_num[cnt++] = cheese?.num;
+                }
+                cnt = 0;
+                american_num = cheese_num[0];
+                mozzarella_num = cheese_num[1];
+                shred_num = cheese_num[2];
+
+                binding.americanStock.text = american_num.toString().plus("개");
+                binding.mozzarellaStock.text = mozzarella_num.toString().plus("개");
+                binding.shredStock.text = shred_num.toString().plus("개");
+            }
+            override fun onCancelled(error: DatabaseError) {
+                print(error.message)
+            }
+        } )
 
         binding.manageMeat.setOnClickListener{
             val nextIntent = Intent(this@ManagerStockBread, ManagerStockMeat::class.java)
             startActivity(nextIntent)
         }
+        binding.manageVege.setOnClickListener{
+            val nextIntent = Intent(this@ManagerStockBread, ManagerStockVege::class.java)
+            startActivity(nextIntent)
+        }
+        binding.manageSauce.setOnClickListener{
+            val nextIntent = Intent(this@ManagerStockBread, ManagerStockSauce::class.java)
+            startActivity(nextIntent)
+        }
 
         binding.whiteMinus.setOnClickListener{
-            white_num -= 1;
-            //post firebase
-            binding.whiteStock.text = white_num.toString().plus("개");
+            white_num = white_num?.minus(1)
+            var key = "white"
+            stockRef.child("bread").child(key).child("num").setValue(white_num)
         }
         binding.whitePlus.setOnClickListener{
-            white_num += 1;
-            //post firebase
-            binding.whiteStock.text = white_num.toString().plus("개");
+            white_num = white_num?.plus(1);
+            var key = "white"
+            stockRef.child("bread").child(key).child("num").setValue(white_num)
         }
 
         binding.honeyMinus.setOnClickListener{
-            honey_num -= 1;
-            //post firebase
-            binding.honeyStock.text = honey_num.toString().plus("개");
+            honey_num = honey_num?.minus(1);
+            var key = "honey"
+            stockRef.child("bread").child(key).child("num").setValue(honey_num)
         }
         binding.honeyPlus.setOnClickListener{
-            honey_num += 1;
-            //post firebase
-            binding.honeyStock.text = honey_num.toString().plus("개");
+            honey_num = honey_num?.plus(1);
+            var key = "honey"
+            stockRef.child("bread").child(key).child("num").setValue(honey_num)
         }
 
         binding.wheetMinus.setOnClickListener{
-            wheet_num -= 1;
-            //post firebase
-            binding.wheetStock.text = wheet_num.toString().plus("개");
+            wheet_num = wheet_num?.minus(1);
+            var key = "wheet"
+            stockRef.child("bread").child(key).child("num").setValue(wheet_num)
         }
         binding.wheetPlus.setOnClickListener{
-            wheet_num += 1;
-            //post firebase
-            binding.wheetStock.text = wheet_num.toString().plus("개");
+            wheet_num = wheet_num?.plus(1);
+            var key = "wheet"
+            stockRef.child("bread").child(key).child("num").setValue(wheet_num)
         }
 
         binding.pamasanMinus.setOnClickListener{
-            pamasan_num -= 1;
-            //post firebase
-            binding.pamasanStock.text = pamasan_num.toString().plus("개");
+            pamasan_num = pamasan_num?.minus(1);
+            var key = "pamasan"
+            stockRef.child("bread").child(key).child("num").setValue(pamasan_num)
         }
         binding.pamasanPlus.setOnClickListener{
-            pamasan_num += 1;
-            //post firebase
-            binding.pamasanStock.text = pamasan_num.toString().plus("개");
+            pamasan_num = pamasan_num?.plus(1);
+            var key = "pamasan"
+            stockRef.child("bread").child(key).child("num").setValue(pamasan_num)
         }
 
         binding.hatiMinus.setOnClickListener{
-            hati_num -= 1;
-            //post firebase
-            binding.hatiStock.text = hati_num.toString().plus("개");
+            hati_num = hati_num?.minus(1);
+            var key = "hati"
+            stockRef.child("bread").child(key).child("num").setValue(hati_num)
         }
         binding.hatiPlus.setOnClickListener{
-            hati_num += 1;
-            //post firebase
-            binding.hatiStock.text = hati_num.toString().plus("개");
+            hati_num = hati_num?.plus(1);
+            var key = "hati"
+            stockRef.child("bread").child(key).child("num").setValue(hati_num)
         }
 
         binding.flatMinus.setOnClickListener{
-            flat_num -= 1;
-            //post firebase
-            binding.flatStock.text = flat_num.toString().plus("개");
+            flat_num = flat_num?.minus(1);
+            var key = "flat"
+            stockRef.child("bread").child(key).child("num").setValue(flat_num)
         }
         binding.flatPlus.setOnClickListener{
-            flat_num += 1;
-            //post firebase
-            binding.flatStock.text = flat_num.toString().plus("개");
+            flat_num = flat_num?.plus(1);
+            var key = "flat"
+            stockRef.child("bread").child(key).child("num").setValue(flat_num)
         }
 
         binding.americanMinus.setOnClickListener{
-            american_num -= 1;
-            //post firebase
-            binding.americanStock.text = american_num.toString().plus("개");
+            american_num = american_num?.minus(1);
+            var key = "american"
+            stockRef.child("cheese").child(key).child("num").setValue(american_num)
         }
         binding.americanPlus.setOnClickListener{
-            american_num += 1;
-            //post firebase
-            binding.americanStock.text = american_num.toString().plus("개");
+            american_num = american_num?.plus(1);
+            var key = "american"
+            stockRef.child("cheese").child(key).child("num").setValue(american_num)
         }
 
         binding.mozzarellaMinus.setOnClickListener{
-            mozzarella_num -= 1;
-            //post firebase
-            binding.mozzarellaStock.text = mozzarella_num.toString().plus("개");
+            mozzarella_num = mozzarella_num?.minus(1);
+            var key = "mozzarella"
+            stockRef.child("cheese").child(key).child("num").setValue(mozzarella_num)
         }
         binding.mozzarellaPlus.setOnClickListener{
-            mozzarella_num += 1;
-            //post firebase
-            binding.mozzarellaStock.text = mozzarella_num.toString().plus("개");
+            mozzarella_num = mozzarella_num?.plus(1);
+            var key = "mozzarella"
+            stockRef.child("cheese").child(key).child("num").setValue(mozzarella_num)
         }
 
         binding.shredMinus.setOnClickListener{
-            shred_num -= 1;
-            //post firebase
-            binding.shredStock.text = shred_num.toString().plus("개");
+            shred_num = shred_num?.minus(1);
+            var key = "shred"
+            stockRef.child("cheese").child(key).child("num").setValue(shred_num)
         }
         binding.shredPlus.setOnClickListener{
-            shred_num += 1;
-            //post firebase
-            binding.shredStock.text = shred_num.toString().plus("개");
+            shred_num = shred_num?.plus(1);
+            var key = "shred"
+            stockRef.child("cheese").child(key).child("num").setValue(shred_num)
+
         }
 
     }
-
-
-
-
 }
--- app/src/main/java/com/example/subway_kiosk/manager/ManagerStockMeat.kt
+++ app/src/main/java/com/example/subway_kiosk/manager/ManagerStockMeat.kt
@@ -2,31 +2,119 @@ package com.example.subway_kiosk.manager
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import com.example.subway_kiosk.R
-import com.example.subway_kiosk.order.MenuSelect
-import com.example.subway_kiosk.order.VegeSelect
-import com.example.subway_kiosk.util.Sandwich
-import kotlin.system.exitProcess
+import com.example.subway_kiosk.databinding.ManagerStockBreadBinding
+import com.example.subway_kiosk.databinding.ManagerStockMeatBinding
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import com.example.subway_kiosk.util.Stock
 
 class ManagerStockMeat : AppCompatActivity()
 {
+    var RootRef = Firebase.database.reference
+    var stockRef = RootRef.child("stock")
+    var meat_num = Array<Int?>(16,{0});
+    var meat_str = arrayOf("avocado","bacon","chicken_breast","chicken_breast_ham","chicken_teriyaki","egg_mayo",
+    "ham","pepperoni","pulled_pork","rotisserie","salami","shrimp","spicy_bbq","steak","tuna")
+    var stock_ary = Array<TextView?>(16,{null})
+    var cnt: Int = 0;
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.manager_stock_meat)
+        val binding = ManagerStockMeatBinding.inflate(layoutInflater)
 
-        val meatBtn = findViewById<Button>(R.id.manage_bread)
-        meatBtn.setOnClickListener {
+        setContentView(binding.root)
+        stock_ary[0] = binding.avocadoStock
+        stock_ary[1] = binding.baconStock
+        stock_ary[2] = binding.chickenBreastStock
+        stock_ary[3] = binding.chickenBreastHamStock
+        stock_ary[4] = binding.chickenTeriyakiStock
+        stock_ary[5] = binding.eggMayoStock
+        stock_ary[6] = binding.hamStock
+        stock_ary[7] = binding.pepperoniStock
+        stock_ary[8] = binding.pulledPorkStock
+        stock_ary[9] = binding.rotisserieStock
+        stock_ary[10] = binding.salamiStock
+        stock_ary[11] = binding.shrimpStock
+        stock_ary[12] = binding.spicyBbqStock
+        stock_ary[13] = binding.steakStock
+        stock_ary[14] = binding.tunaStock
+
+        stockRef.child("meat").addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (item in snapshot.children) {
+                    var meat : Stock? = item.getValue(Stock::class.java)
+                    meat_num[cnt] = meat?.num;
+                    stock_ary[cnt]?.text = meat_num[cnt].toString().plus("개");
+                    cnt++;
+                }
+                cnt = 0;
+            }
+            override fun onCancelled(error: DatabaseError) {
+                print(error.message)
+            }
+        } )
+
+        binding.manageBread.setOnClickListener{
             val nextIntent = Intent(this@ManagerStockMeat, ManagerStockBread::class.java)
             startActivity(nextIntent)
         }
+        binding.manageVege.setOnClickListener{
+            val nextIntent = Intent(this@ManagerStockMeat, ManagerStockVege::class.java)
+            startActivity(nextIntent)
+        }
+        binding.manageSauce.setOnClickListener{
+            val nextIntent = Intent(this@ManagerStockMeat, ManagerStockSauce::class.java)
+            startActivity(nextIntent)
+        }
+        binding.avocadoMinus.setOnClickListener{ minus(0) }
+        binding.avocadoPlus.setOnClickListener{plus(0)}
+        binding.baconMinus.setOnClickListener{ minus(1) }
+        binding.baconPlus.setOnClickListener{plus(1)}
+        binding.chickenBreastMinus.setOnClickListener{ minus(2) }
+        binding.chickenBreastPlus.setOnClickListener{plus(2)}
+        binding.chickenBreastHamMinus.setOnClickListener{ minus(3) }
+        binding.chickenBreastHamPlus.setOnClickListener{plus(3)}
+        binding.chickenTeriyakiMinus.setOnClickListener{ minus(4) }
+        binding.chickenTeriyakiPlus.setOnClickListener{plus(4)}
+        binding.eggMayoMinus.setOnClickListener{ minus(5) }
+        binding.eggMayoPlus.setOnClickListener{plus(5)}
+        binding.hamMinus.setOnClickListener{ minus(6) }
+        binding.hamPlus.setOnClickListener{plus(6)}
+        binding.pepperoniMinus.setOnClickListener{ minus(7) }
+        binding.pepperoniPlus.setOnClickListener{plus(7)}
+        binding.pulledPorkMinus.setOnClickListener{ minus(8) }
+        binding.pulledPorkPlus.setOnClickListener{plus(8)}
+        binding.rotisserieMinus.setOnClickListener{ minus(9) }
+        binding.rotisseriePlus.setOnClickListener{plus(9)}
+        binding.salamiMinus.setOnClickListener{ minus(10) }
+        binding.salamiPlus.setOnClickListener{plus(10)}
+        binding.shrimpMinus.setOnClickListener{ minus(11) }
+        binding.shrimpPlus.setOnClickListener{plus(11)}
+        binding.spicyBbqMinus.setOnClickListener{ minus(12) }
+        binding.spicyBbqPlus.setOnClickListener{plus(12)}
+        binding.steakMinus.setOnClickListener{ minus(13) }
+        binding.steakPlus.setOnClickListener{plus(13)}
+        binding.tunaMinus.setOnClickListener{ minus(14) }
+        binding.tunaPlus.setOnClickListener{plus(14)}
     }
-
-
-
+    fun minus(i: Int){
+        meat_num[i] = meat_num[i]?.minus(1)
+        stockRef.child("meat").child(meat_str[i]).child("num").setValue(meat_num[i])
+    }
+    fun plus(i:Int){
+        meat_num[i] = meat_num[i]?.plus(1)
+        stockRef.child("meat").child(meat_str[i]).child("num").setValue(meat_num[i])
+    }
 }
+
