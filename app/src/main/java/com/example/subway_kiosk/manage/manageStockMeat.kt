29d875ba1016cf3e665b5768510b83d0116ab38e
Minj1031 <109937806+Minj1031@users.noreply.github.com>
2022-12-13 15:41:49 +0900

    manager_stock_bread

---
+++ app/src/main/java/com/example/subway_kiosk/manage/manageStockMeat.kt
@@ -0,0 +1,32 @@
+package com.example.subway_kiosk.manage
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.subway_kiosk.R
+import com.example.subway_kiosk.order.MenuSelect
+import com.example.subway_kiosk.order.VegeSelect
+import com.example.subway_kiosk.util.Sandwich
+import kotlin.system.exitProcess
+
+class manageStockMeat : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.manager_stock_meat)
+
+        val meatBtn = findViewById<Button>(R.id.manage_bread)
+        meatBtn.setOnClickListener {
+            val nextIntent = Intent(this@manageStockMeat, manageStockBread::class.java)
+            startActivity(nextIntent)
+        }
+    }
+
+
+
+}
