29317c707b3ad47ddd9b01a9367dfc29204fd682
Wonseok Han <ricky2101@naver.com>
2022-12-15 05:49:25 +0900

    home

--- app/src/main/java/com/example/subway_kiosk/MainActivity.kt
+++ app/src/main/java/com/example/subway_kiosk/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.example.subway_kiosk.databinding.ActivityMainBinding
 import com.example.subway_kiosk.order.MeatSelect
 import com.example.subway_kiosk.util.Sandwich
 
@@ -15,7 +16,9 @@ class MainActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.first_page)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val regularorder = findViewById<Button>(R.id.regular_order)
         val shop = findViewById<TextView>(R.id.shopping_cart)
